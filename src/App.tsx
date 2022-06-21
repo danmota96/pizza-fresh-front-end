8d3ffe7d8d4bcaab05f41a14b2e6740faf1b7d3d
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-21 14:06:02 -0300

    install router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css';
-import { useState } from "react";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "assets/styles/theme";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-
-  const [numero, setNumero] = useState<number>(0);
- 
-
-  function addNumber () {
-    setNumero(numero + 1);
-  }
-  
   return (
-    <div className="App">
-     <h1>{numero}</h1>
-     <button onClick={addNumber}>Clique</button>
-    </div>
+    /* implementar o tema criado na pasta styles */
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <QueryClientProvider client={new QueryClient}>
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
