9e229c35568e39f2e57c6ba8bee87fa00c6befe3
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-21 15:12:20 -0300

    add(home): implement 'home' without components

--- src/router.tsx
+++ src/router.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home/index";
 const Router = () => {
     return (
         <Routes>
-            <Route path={RoutePath.HOME} element={<Home />} />
+            <Route path={RoutePath.HOME} element={<Home/>} />
         </Routes>
     );
 }
