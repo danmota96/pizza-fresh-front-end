a4573b52c40d57357fe0d1f1080efd7b4da8229c
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-06-30 22:29:02 -0300

    feat(menu): implement menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,39 @@
+import logo from "assets/imgs/logo.png";
+import * as S from "./style";
+import { ReactComponent as Logout } from 'assets/icons/logout.svg';
+import { RoutePath } from "types/routes";
+import { NavItem } from "./types";
+
+//propriedades do menu
+interface MenuProps {
+  active: RoutePath;
+  navItems: NavItem[];
+}
+
+//menu lateral
+const Menu = ({ active, navItems }: MenuProps) => {
+  return (
+    <S.Menu>
+	<nav>
+		<S.MenuLogo>
+      <img src={logo} alt="Logo" />
+    </S.MenuLogo>
+    {/* percorrer o array */}
+		{navItems.map((item, index) => (
+
+			<S.MenuItem key={`MenuItem-${index}`} active={item.path === active}>
+        
+				<S.MenuItemButton active={item.path === active}>
+					{item.icon}
+				</S.MenuItemButton>
+
+			</S.MenuItem>
+
+		))}
+	</nav>
+	<S.MenuItemLogout>...</S.MenuItemLogout>
+</S.Menu>
+  );
+};
+
+export default Menu;
