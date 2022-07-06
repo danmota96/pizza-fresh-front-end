ffef1ed3d90b5bfea75329c13787de6c912f092b
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-07-05 21:29:22 -0300

    feat(productItemList): implement component productItemList at homepage

---
+++ src/components/ProductItemList/index.tsx
@@ -0,0 +1,23 @@
+import * as S from "./style";
+
+const ProductItemList = () => {
+  return (
+    <section>
+      <S.ProductItemListHeader>
+        <S.ProductItemListHeaderTitle>
+          Escolha os Sabores
+        </S.ProductItemListHeaderTitle>
+        <S.ProductItemListHeaderSelect>
+          <option value="default">Selecione a mesa</option>
+          <option value="">Mesa</option>
+        </S.ProductItemListHeaderSelect>
+      </S.ProductItemListHeader>
+      
+      <S.ProductItemList>
+
+      </S.ProductItemList>
+    </section>
+  );
+};
+
+export default ProductItemList;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,54 +1,51 @@
 import * as S from "./style";
 import { ReactComponent as Search } from "assets/icons/search.svg";
-import Menu from 'components/Menu';
+import Menu from "components/Menu";
 import { RoutePath } from "types/routes";
 import { navigationItems } from "data/navigation";
 import { DateTime } from "luxon";
+import ProductItemList from "components/ProductItemList";
 
 const Home = () => {
-	const dateDescription = DateTime.now().toLocaleString({
-		...DateTime.DATE_SHORT,
-		weekday: "long",
-	  });
-	return (
-		<S.Home>
-			{/* IMPORTAÇÃO DA TAG MENU */}
-            <Menu
-			active={RoutePath.HOME}
-			navItems={navigationItems}
-				/>
-			<S.HomeContent>
-				<header>
-					<S.HomeHeaderDetails>
-						<div>	
-					
-							<S.HomeHeaderDetailsLogo>
-								Pizza 
-							</S.HomeHeaderDetailsLogo>
-							<S.HomeHeaderDetailsDate>
-							{dateDescription}
-							</S.HomeHeaderDetailsDate>
-						</div>
-						<S.HomeHeaderDetailsSearch>
-                            <Search />
-							<input type="text" placeholder="Procure pelo sabor" />
-						</S.HomeHeaderDetailsSearch>
-					</S.HomeHeaderDetails>
-				</header>
-				<div>
-					<S.HomeProductTitle>
-						<b>Pizzas</b>
-					</S.HomeProductTitle>
-					<S.HomeProductList>
-						<p>Lista de produtos aqui</p>
-					</S.HomeProductList>
-				</div>
-			</S.HomeContent>
-			<aside>
-				<p>Detalhes dos pedidos aqui</p>
-			</aside>
-		</S.Home>
-	);
+  const dateDescription = DateTime.now().toLocaleString({
+    ...DateTime.DATE_SHORT,
+    weekday: "long",
+  });
+  return (
+    <S.Home>
+      {/* IMPORTAÇÃO DA TAG MENU */}
+      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <S.HomeContent>
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>Pizza</S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                {dateDescription}
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procure pelo sabor" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Pizzas</b>
+          </S.HomeProductTitle>
+
+          <S.HomeProductList>
+            <ProductItemList></ProductItemList>
+          </S.HomeProductList>
+
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
 };
 
 export default Home;
