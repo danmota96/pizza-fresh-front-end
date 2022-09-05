1d5450aca46c52b83325a597491162a6cc1627ba
Daniel Mota <97922515+danmota96@users.noreply.github.com>
2022-09-05 15:37:17 -0300

    feat(components): implement MangeUser & EditUser

--- src/router.tsx
+++ src/router.tsx
@@ -4,14 +4,17 @@ import Home from "./pages/Home/index";
 import Login from 'pages/Login';
 import Settings from 'pages/Settings';
 import ManageProducts from 'components/ManageProducts';
+import ManageUsers from 'components/ManageUsers';
 
 const Router = () => {
     return (
         <Routes>
             <Route path={RoutePath.HOME} element={<Home/>} />
             <Route path={RoutePath.LOGIN} element={<Login/>} />
-            <Route path={RoutePath.SETTINGS} element={<Settings />} />
-            <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+            <Route path={RoutePath.SETTINGS} element={<Settings />}>
+                <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+                <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+            </Route>
         </Routes>
     );
 }
